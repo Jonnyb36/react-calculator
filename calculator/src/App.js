b9c12ebcf2eb34a16931ddaea453fbf279275a76
Jon Brown <jonnyb36@hotmail.com>
2018-12-29 23:10:05 +0000

    Revert "Updates"
    This reverts commit c78886d1a6a4c40a02b80047499ea8882d735cef.

---
+++ calculator/src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'reactstrap';
+//import logo from './logo.svg';
+import './App.css';
+import {Calculator_Screen} from './Calculator_Screen';
+import {Calculator_Body} from './Calculator_Body';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      screenValue: 0,
+      calcString: "",
+      action: null,
+      numberSelectedLast: false
+    };
+  }
+
+  buttonOnClick = e => {
+    const numRegex = /\d|\./;
+    const numberSelected = numRegex.test(e.target.value);//true for numbers
+    
+    let newValue, calcString;
+    if(numberSelected) {
+      if (!this.state.numberSelectedLast){
+        newValue = e.target.value;
+        if(this.state.action){
+          calcString = this.state.calcString + this.state.action;
+          this.setState({
+            calcString: calcString
+          });
+        }  
+      } else {
+        newValue = String(this.state.screenValue) + String(e.target.value);
+      }
+      this.setState({
+        numberSelectedLast: true,
+        screenValue: newValue
+      });
+
+    } else if (e.target.value === "=") {
+      const calcValue = eval(this.state.calcString + this.state.screenValue);
+      this.setState({
+        numberSelectedLast: false,
+        screenValue: calcValue,
+        calcString: String(calcValue),
+        action: null
+      });
+
+    } else if (e.target.value === "ce") {
+      this.setState({
+        numberSelectedLast: false,
+        screenValue: 0,
+        calcString: "",
+        action: null
+      });
+      
+    } else {
+      const newValue = this.state.numberSelectedLast? eval(this.state.calcString + String(this.state.screenValue)): this.state.calcString
+      this.setState({
+        numberSelectedLast: false,
+        action: e.target.value,
+        screenValue: newValue,
+        calcString: newValue
+      });
+    }
+  }
+  
+  render() {
+    return (
+        <Container className="App">
+              <Row style={{height:30}}/>
+              <Row>
+                <Col md={{ size: 4, offset: 4 }}>
+                  <Calculator_Screen id="display" value={this.state.screenValue}/>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ size: 4, offset: 4 }}>
+                  <Calculator_Body onClick={this.buttonOnClick}/>
+                </Col>
+              </Row>
+        </Container>
+    );
+  }
+}
