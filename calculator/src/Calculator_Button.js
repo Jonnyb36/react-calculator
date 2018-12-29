c78886d1a6a4c40a02b80047499ea8882d735cef
Jon Brown <jonnyb36@hotmail.com>
2018-12-29 23:04:31 +0000

    Updates

--- calculator/src/Calculator_Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Calculator_Button = props => {
-    return <button onClick={props.onClick} value={props.value}>
-            {props.value}
-        </button>
-}  
--- calculator/src/Calculator_Screen.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Calculator_Screen = props => {
-    return <div className="screen">{props.value}</div>
-}
